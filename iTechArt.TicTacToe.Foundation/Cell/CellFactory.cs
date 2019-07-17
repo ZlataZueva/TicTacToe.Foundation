863ce605478acc265602e38f5f4831af0b60b939
Zlata <zlatka.zueva@gmail.com>
2019-07-17 15:15:18 +0300

    Implement `ICellFactory` by `CellFactory`

---
+++ iTechArt.TicTacToe.Foundation/Cell/CellFactory.cs
@@ -0,0 +1,13 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.Foundation.Cell
+{
+    public class CellFactory : ICellFactory
+    {
+        public ICell CreateCell(int row, int column)
+        {
+            
+            return new Cell(row, column);
+        }
+    }
+}
