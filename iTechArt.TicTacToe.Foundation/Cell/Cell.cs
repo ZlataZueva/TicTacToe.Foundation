c11ea610ba0187385e4c9e2229bc6d5d398356f6
Zlata <zlatka.zueva@gmail.com>
2019-07-18 13:08:21 +0300

    Implement Figure setter in Cell explicitly

--- iTechArt.TicTacToe.Foundation/Cell/Cell.cs
+++ iTechArt.TicTacToe.Foundation/Cell/Cell.cs
@@ -1,16 +1,33 @@
-﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+﻿using System;
+using iTechArt.TicTacToe.Foundation.Interfaces;
 
 namespace iTechArt.TicTacToe.Foundation.Cell
 {
-    public class Cell : IFigureSettableCell
+    public class Cell : ICellInternal
     {
         public int Row { get; }
 
         public int Column { get; }
 
         public bool IsEmpty => Figure == null;
-        
-        public IFigure Figure { get; set; }
+
+        public IFigure Figure { get; private set; }
+
+
+        IFigure ICellInternal.Figure
+        {
+            set
+            {
+                if (IsEmpty)
+                {
+                    Figure = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Cell is occupied");
+                }
+            }
+        }
 
 
         public Cell(int row, int column)
