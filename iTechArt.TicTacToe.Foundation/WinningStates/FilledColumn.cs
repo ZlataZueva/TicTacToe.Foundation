72382f6716e07788a62be93b1df04c88f557530d
Zlata <zlatka.zueva@gmail.com>
2019-07-22 13:31:59 +0300

    Remove useless Number properties

--- iTechArt.TicTacToe.Foundation/WinningStates/FilledColumn.cs
+++ iTechArt.TicTacToe.Foundation/WinningStates/FilledColumn.cs
@@ -4,13 +4,10 @@ namespace iTechArt.TicTacToe.Foundation.WinningStates
 {
     public class FilledColumn : WinningState
     {
-        public int Number { get; }
-
-
         public FilledColumn(IBoard board, int number)
             : base(board, cell => cell.Column == number)
         {
-            Number = number;
+
         }
     }
 }
--- iTechArt.TicTacToe.Foundation/WinningStates/FilledRow.cs
+++ iTechArt.TicTacToe.Foundation/WinningStates/FilledRow.cs
@@ -4,13 +4,10 @@ namespace iTechArt.TicTacToe.Foundation.WinningStates
 {
     public class FilledRow : WinningState
     {
-        public int Number { get; }
-
-
         public FilledRow(IBoard board, int number)
             : base(board, cell => cell.Row == number)
         {
-            Number = number;
+
         }
     }
 }
