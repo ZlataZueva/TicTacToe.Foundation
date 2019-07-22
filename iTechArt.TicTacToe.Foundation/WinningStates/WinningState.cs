b0ab1d29fb51d0f01dd7b25bda4b086cdd50efad
Zlata <zlatka.zueva@gmail.com>
2019-07-22 16:53:09 +0300

    Optimize calculation of winning state activation
    Use caching

--- iTechArt.TicTacToe.Foundation/WinningStates/WinningState.cs
+++ iTechArt.TicTacToe.Foundation/WinningStates/WinningState.cs
@@ -7,15 +7,38 @@ namespace iTechArt.TicTacToe.Foundation.WinningStates
 {
     public abstract class WinningState : IWinningState
     {
+        private bool _isPossible;
+        private bool _isActivated;
+
+
         public IReadOnlyCollection<ICell> Cells { get; }
-        
-        public bool IsActive => Cells.Where(c => !c.IsEmpty).Select(c => c.Figure.Type)
-                                     .Distinct().Count() == 1;
+
+        public bool IsActive
+        {
+            get
+            {
+                if (!_isPossible || _isActivated)
+                {
+                    return _isActivated;
+                }
+                var figureTypesCounter = Cells.Where(c => !c.IsEmpty)
+                    .Select(c => c.Figure.Type).Distinct().Count();
+                if (figureTypesCounter > 1)
+                {
+                    return _isPossible = false;
+                }
+                
+                return _isActivated = Cells.All(c => !c.IsEmpty);
+            }
+        } 
 
 
         protected WinningState(IBoard board, Func<ICell, bool> filter)
         {
             Cells = board.Where(filter).ToList();
+
+            _isActivated = false;
+            _isPossible = true;
         }
     }
 }
