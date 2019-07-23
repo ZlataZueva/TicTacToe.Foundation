8193aea13e3c1d9ea75f982dbef755f8feb370d2
Zlata <zlatka.zueva@gmail.com>
2019-07-23 13:31:23 +0300

    Use bool? instead of 2 bool flags

--- iTechArt.TicTacToe.Foundation/WinningStates/WinningStatesFactory.cs
+++ iTechArt.TicTacToe.Foundation/WinningStates/WinningStatesFactory.cs
@@ -8,9 +8,9 @@ namespace iTechArt.TicTacToe.Foundation.WinningStates
     {
         public IReadOnlyCollection<IWinningState> CreateWinningStates(IBoard board)
         {
-            var columns = Enumerable.Range(0, board.Size).Select<int, IWinningState>(n => new FilledColumn(board, n));
-            var rows = Enumerable.Range(0, board.Size).Select<int, IWinningState>(n => new FilledRow(board, n));
-            var diagonals = new IWinningState[] {new FilledMajorDiagonal(board), new FilledMinorDiagonal(board)};
+            var columns = Enumerable.Range(0, board.Size).Select<int, IWinningState>(c => new FilledColumn(board, c));
+            var rows = Enumerable.Range(0, board.Size).Select<int, IWinningState>(r => new FilledRow(board, r));
+            var diagonals = new IWinningState[] { new FilledMajorDiagonal(board), new FilledMinorDiagonal(board) };
 
             return columns.Concat(rows).Concat(diagonals).ToList();
         }
