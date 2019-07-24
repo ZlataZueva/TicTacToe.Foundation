2d36fea522199ce3eee91b1a8f3c4fdcea6b16ae
Zlata <zlatka.zueva@gmail.com>
2019-07-24 18:27:02 +0300

    Create game configuration factory with validation

---
+++ iTechArt.TicTacToe.Foundation/Game/GameConfigurationFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.Foundation.Game
+{
+    public class GameConfigurationFactory : IGameConfigurationFactory
+    {
+        public IGameConfiguration CreateGameConfiguration(
+            IReadOnlyCollection<IPlayer> players, 
+            int firstPlayerIndex, 
+            int boardSize)
+        {
+            var firstPlayer = players.ElementAt(firstPlayerIndex);
+            var uniquePlayers = players.Distinct().ToList();
+            if (uniquePlayers.Select(p => p.FigureType).Distinct().Count() != 1)
+            {
+                throw new ArgumentException("Players should have unique figure types");
+            }
+            
+            return new GameConfiguration(uniquePlayers, uniquePlayers.IndexOf(firstPlayer), boardSize);
+        }
+    }
+}
---
+++ iTechArt.TicTacToe.Foundation/Interfaces/IGameConfigurationFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace iTechArt.TicTacToe.Foundation.Interfaces
+{
+    public interface IGameConfigurationFactory
+    {
+        IGameConfiguration CreateGameConfiguration(
+            IReadOnlyCollection<IPlayer> players, 
+            int firstPlayerIndex, 
+            int boardSize);
+    }
+}
