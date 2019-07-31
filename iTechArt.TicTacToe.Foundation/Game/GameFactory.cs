e38aa146b4abdcaf27f9d40ee0d00fdc93e8b7f4
Zlata <zlatka.zueva@gmail.com>
2019-07-31 12:13:42 +0300

    Implement game factory

---
+++ iTechArt.TicTacToe.Foundation/Game/GameFactory.cs
@@ -0,0 +1,28 @@
+﻿using iTechArt.TicTacToe.Foundation.Interfaces;
+
+namespace iTechArt.TicTacToe.Foundation.Game
+{
+    public class GameFactory : IGameFactory
+    {
+        private readonly IBoardFactory _boardFactory;
+        private readonly IWinningStatesFactory _winningStatesFactory;
+        private readonly IGameInputProvider _gameInputProvider;
+
+
+        public GameFactory(
+            IBoardFactory boardFactory, 
+            IWinningStatesFactory winningStatesFactory, 
+            IGameInputProvider gameInputProvider)
+        {
+            _boardFactory = boardFactory;
+            _winningStatesFactory = winningStatesFactory;
+            _gameInputProvider = gameInputProvider;
+        }
+
+
+        public IGame CreateGame(IGameConfiguration gameConfiguration)
+        {
+            return new Game(gameConfiguration, _boardFactory, _winningStatesFactory, _gameInputProvider);
+        }
+    }
+}
---
+++ iTechArt.TicTacToe.Foundation/Interfaces/IGameFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace iTechArt.TicTacToe.Foundation.Interfaces
+{
+    public interface IGameFactory
+    {
+        IGame CreateGame(IGameConfiguration gameConfiguration);
+    }
+}
